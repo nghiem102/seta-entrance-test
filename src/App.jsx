fbd211beb6d5c011932d23142e067faf94b3d294
nghiem102 <ledinhnghiem102@gmail.com>
2022-05-20 12:11:55 +0700

    update question 1

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,37 @@ import { createPost, getPosts } from "./features/postSlice"
 function App() {
 
   //javascrip test
-  const array1 = ['a', 'ab', 'abc', 'cd', 'def', 'gh' ]
+  const array1 = ['a', 'ab', 'abc', 'cd', 'def', 'gh']
   const array2 = [1, 4, 2, 3, 5]
 
   // question 1
   const myFunction = (array) => {
-    const arraySortByLength = array.map(item => item.length)
-    const groupByArray = arraySortByLength.reduce((a, b) => ({ ...a, [b]: (a[b] || 0) + 1 }), {})
-    const arrayLength = Object.entries(groupByArray).sort((a, b) => {
-      const [keyA,valueA] = a
-      const [keyB,valueB] = b
-      return valueB - valueA
-    })
-    const [key,value] = arrayLength[0]
+    // const arraySortByLength = array.map(item => item.length)
+    // const groupByArray = arraySortByLength.reduce((a, b) => ({ ...a, [b]: (a[b] || 0) + 1 }), {})
+    // const arrayLength = Object.entries(groupByArray).sort((a, b) => {
+    //   const [keyA,valueA] = a
+    //   const [keyB,valueB] = b
+    //   return valueB - valueA
+    // })
+    // const [key,value] = arrayLength[0]
 
-    const result = array.filter(item => item.length === Number(key))
+    // const result = array.filter(item => item.length === Number(key))
+    // console.log(result);
+
+    let max = 0;
+    let index = -1;
+    let counter = {};
+
+    array.map((item) => item.length)
+      .forEach((length) => {
+        const count = (counter[length] || 0) + 1;
+        if (max < count) {
+          max = count;
+          index = length;
+        }
+        counter[length] = count;
+      });
+      const result = array.filter(item => item.length === Number(index))
     console.log(result);
   }
 
@@ -30,8 +46,8 @@ function App() {
 
   //question 2
   const sum = (array) => {
-  const sortArray = array.sort((a,b) => {
-      return b-a
+    const sortArray = array.sort((a, b) => {
+      return b - a
     })
     const sum = sortArray[0] + sortArray[1]
     return sum
